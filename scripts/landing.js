8c80376ed924cc8effc6d2b1dc7aa761b595ce14
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-06-07 00:04:33 -0700

    Checkpoint 9 DOM Scripting: Events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -14,6 +14,11 @@ var animatePoints = function(points) {
 };
 
 window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+      animatePoints(pointsArray);
+    }
+
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
