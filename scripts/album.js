dc580adef3099f6a743365789436f8257b1344c4
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-06-13 04:31:14 -0700

    Checkpoint 12 play pause part 1 completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,10 +30,26 @@ var albumMarconi = {
     ]
 };
 
+// My Example Album
+var albumBleedAmerican = {
+    title: 'Bleed American',
+    artist: 'Jimmy Eat World',
+    label: 'DreamWorks',
+    year: '2001',
+    albumArtUrl: 'assets/images/album_covers/12.png',
+    songs: [
+        { title: 'Bleed American', duration: '3:02' },
+        { title: 'A Praise Chorus', duration: '4:03' },
+        { title: 'The Middle', duration: '2:46'},
+        { title: 'Your House', duration: '4:46' },
+        { title: 'Sweetness', duration: '3:40'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -42,29 +58,63 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+// #1 -- Select elements that we want to populate with text dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-    // #2
+var setCurrentAlbum = function(album) {
+    // #2 -- Assign values to each part of the album list (text, images)
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
 
-    // #3
+    // #3 -- Clear contents of album song list container
     albumSongList.innerHTML = '';
 
-    // #4
+    // #4 -- Build list of songs from album Javascript object
     for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
 
+// Elements we'll be adding listeners to
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+    // #1
+    // Only target individual song rows during event delegation
+    if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+        // Revert the content back to the number
+        // Selects first child element, which is the song-item-number element
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+
+    var albums = [albumPicasso, albumMarconi, albumBleedAmerican];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+      // console.log('click event happens')
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
 };
