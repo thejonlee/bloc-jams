d02ec308e18da9d5f1a8b83511c8792fad3c6e91
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-06-16 02:48:37 -0700

    Checkpoint 16 jQuery landing page: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,28 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
 
-var animatePoints = function(points) {
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-
-    for (var i=0; i<points.length; i++) {
-      revealPoint(i);
-    }
+    var revealPoint = function() {
+        // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    // #6
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-    if (window.innerHeight > 950) {
-      animatePoints(pointsArray);
+$(window).load(function() {
+    // #1
+    if ($(window).height() > 950) {
+      animatePoints();
     }
-
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    // #2
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    // #3
+    $(window).scroll(function(event) {
+        // #4
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}
+});
