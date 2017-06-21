e9dcbb74bc562d14b8eab529e0ee40fc61081554
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-06-21 00:29:03 -0700

    Checkpoint 17 collection and album views: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+// #1 -- convention to start jquery function name with verb
+var buildCollectionItemTemplate = function() {
+  var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -13,15 +15,20 @@ var collectionItemTemplate =
   + '  </div>'
   + '</div>'
   ;
+  // #2 -- "future proof" template by wrapping in a jquery object
+  return $(template);
+};
 
 
-window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
+$(window).load(function() {
     // #3
+    var $collectionContainer = $('.album-covers');
+    // #4
+    $collectionContainer.empty();
+
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        // #5
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
